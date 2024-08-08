dea7bc12db53f0005590507af80af88ccab4ca55
Matias Pinho <mati1234661mto@gmail.com>
2024-08-08 04:11:25 -0300

    test: introduce fixable linting issue for workflow validation
    - Add a linting issue that can be automatically fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import './App.css';
+import "./App.css";
 
 function App() {
 	return (
 		<>
 			<h1>testeo</h1>
-			<button type='button'> Hola </button>
-			<button type='button'> Holas </button>
-			<a href='./' target='_blank'>
+			<button type="button"> Hola </button>
+			<button type="button"> Holas </button>
+			<a href="./" target="_blank">
 				hola
 			</a>
 		</>
